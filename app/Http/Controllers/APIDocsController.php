5d92c1fdae80366117a0788fc206a8c499fe526f
Cary <fdjkgh580@gmail.com>
2020-03-08 19:31:30 +0800

    加入 Swagger 用來設計 API Document 加入 Echo 用來測試使用 加入 LoginController 準備建立登入方法

---
+++ app/Http/Controllers/APIDocsController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+/**
+ * @OA\Info(title="面試趣 APIs Document", version="0.1")
+ */
+class APIDocsController extends Controller
+{
+    public function getJSON()
+    {
+        //指定Swagger要掃描的路徑，\OpenApi\scan()會讀取這個路徑下的文件
+        //並且將讀取到的Swagger Annotations都轉換成JSON格式
+        $swagger = \OpenApi\scan(app_path('Http/Controllers/'));
+        return response()->json($swagger, 200);
+    }
+}
